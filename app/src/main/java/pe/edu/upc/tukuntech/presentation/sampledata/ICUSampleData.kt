1b030f0801c6ca64f38feebe92c51761dc74b02b
Oscar Soto <oscaredu01@hotmail.com>
2025-05-14 02:15:06 -0500

    chore(sample): add sample ICU patient data UI/testing

---
+++ app/src/main/java/pe/edu/upc/tukuntech/presentation/sampledata/ICUSampleData.kt
@@ -0,0 +1,9 @@
+package pe.edu.upc.tukuntech.presentation.sampledata
+
+import pe.edu.upc.tukuntech.domain.models.Patient
+
+val sampleIcuPatients = listOf(
+    Patient("Cama 101 : Juan Torres", 72, "120 / 80", 97, 36.8),
+    Patient("Cama 102 : Maria López", 78, "125 / 82", 96, 36.9),
+    Patient("Cama 103 : Carlos Díaz", 88, "130 / 85", 95, 37.2)
+)
