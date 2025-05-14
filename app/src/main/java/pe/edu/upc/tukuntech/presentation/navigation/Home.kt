783510a34035a1d38fb70a10429397d47b066c17
Oscar Soto <oscaredu01@hotmail.com>
2025-05-14 02:14:12 -0500

    feat(routing): configure Home.kt and add navigation routes

---
+++ app/src/main/java/pe/edu/upc/tukuntech/presentation/navigation/Home.kt
@@ -0,0 +1,108 @@
+package pe.edu.upc.tukuntech.presentation.navigation
+
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.FavoriteBorder
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material3.BottomAppBar
+import androidx.compose.material3.Icon
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import pe.edu.upc.tukuntech.presentation.views.ClinicHomeView
+import pe.edu.upc.tukuntech.presentation.views.ICUView
+import pe.edu.upc.tukuntech.presentation.views.PostOperativeView
+
+@Composable
+fun Home() {
+
+    val navController = rememberNavController()
+
+    val navigationItems = listOf(
+        NavigationItem(
+            icon = Icons.Default.Home,
+            title = "Home",
+            route = "home"
+        ),
+        NavigationItem(
+            icon = Icons.Default.Favorite,
+            title = "PostOperative",
+            route = "post_operative"
+        ),
+        NavigationItem(
+            icon = Icons.Default.FavoriteBorder,
+            title = "UCI",
+            route = "uci"
+        ),
+        NavigationItem(
+            icon = Icons.Default.Person,
+            title = "Patients",
+            route = "patients"
+        )
+    )
+
+    val selectedIndex = remember {
+        mutableStateOf(0)
+    }
+
+    Scaffold(
+        bottomBar = {
+            BottomAppBar {
+                navigationItems.forEachIndexed { index, item ->
+                    NavigationBarItem(
+                        selected = selectedIndex.value == index,
+                        icon = {
+                            Icon(item.icon, contentDescription = null)
+                        },
+                        onClick = {
+                            selectedIndex.value = index
+                            navController.navigate(item.route)
+                        },
+                        label = {
+                            Text(item.title)
+                        }
+                    )
+                }
+            }
+        }
+    ) { padding ->
+        NavHost(
+            navController,
+            startDestination = "home",
+            modifier = Modifier.padding(padding)
+        ) {
+            composable("home") {
+                ClinicHomeView(navController)
+
+            }
+            composable("post_operative") {
+                PostOperativeView()
+
+            }
+            composable("uci") {
+                ICUView()
+
+            }
+            composable("patients") {
+
+            }
+        }
+    }
+}
+
+data class NavigationItem(
+    val icon: ImageVector,
+    val title: String,
+    val route: String
+)
+
