ef0d51e2b166e32aba672ce0c4ad96fc0497cb50
Oscar Soto <oscaredu01@hotmail.com>
2025-05-14 02:13:23 -0500

    feat(navigation): redirect MainActivity to Home screen

--- app/src/main/java/pe/edu/upc/tukuntech/MainActivity.kt
+++ app/src/main/java/pe/edu/upc/tukuntech/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import pe.edu.upc.tukuntech.presentation.navigation.Home
 import pe.edu.upc.tukuntech.ui.theme.TukunTechTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,29 +20,9 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             TukunTechTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                Home()
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    TukunTechTheme {
-        Greeting("Android")
-    }
-}
