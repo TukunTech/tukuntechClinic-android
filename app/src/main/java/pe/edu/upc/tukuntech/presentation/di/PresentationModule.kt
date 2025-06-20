951b827368ff39d223a7b1fc31c1f5a4777c9a6e
Rafooo6 <rafaelbravo032@gmail.com>
2025-06-19 20:47:01 -0500

    chore(di): provide AuthViewModel and AuthService via PresentationModule.

---
+++ app/src/main/java/pe/edu/upc/tukuntech/presentation/di/PresentationModule.kt
@@ -0,0 +1,21 @@
+package pe.edu.upc.tukuntech.presentation.di
+
+import pe.edu.upc.tukuntech.data.remote.AuthService
+import pe.edu.upc.tukuntech.data.repository.AuthRepository
+import pe.edu.upc.tukuntech.presentation.viewmodels.AuthViewModel
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+object PresentationModule {
+    fun getAuthService(): AuthService {
+    return Retrofit.Builder()
+        .baseUrl("https://tukun-tech-platform.onrender.com")
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
+        .create(AuthService::class.java)
+}
+
+    fun getAuthViewModel(): AuthViewModel {
+        return AuthViewModel(AuthRepository(getAuthService()))
+    }
+}
