0f3e359b4c5972ff24bd5067353d17cdf0cbf0c8
Erick <erickmaraza20@gmail.com>
2025-06-20 00:28:52 -0500

    feat(auth): add register endpoint to AuthService

--- app/src/main/java/pe/edu/upc/tukuntech/data/remote/AuthService.kt
+++ app/src/main/java/pe/edu/upc/tukuntech/data/remote/AuthService.kt
@@ -3,6 +3,7 @@ package pe.edu.upc.tukuntech.data.remote
 
 import pe.edu.upc.tukuntech.data.models.LoginRequest
 import pe.edu.upc.tukuntech.data.models.LoginResponse
+import pe.edu.upc.tukuntech.data.models.RegisterRequest
 import retrofit2.Response
 import retrofit2.http.Body
 import retrofit2.http.POST
@@ -10,4 +11,6 @@ import retrofit2.http.POST
 interface AuthService {
     @POST("/api/v1/auth/login")
     suspend fun login(@Body request: LoginRequest): Response<LoginResponse>
+    @POST("/api/v1/auth/register")
+    suspend fun register(@Body request: RegisterRequest): Response<Void>
 }
