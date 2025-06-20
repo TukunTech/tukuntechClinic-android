8074914b2536fd486aebd4b6e020f4e1d5d22cca
Rafooo6 <rafaelbravo032@gmail.com>
2025-06-19 20:46:21 -0500

    feat(auth): implement AuthRepository to handle login request logic

---
+++ app/src/main/java/pe/edu/upc/tukuntech/data/repository/AuthRepository.kt
@@ -0,0 +1,22 @@
+package pe.edu.upc.tukuntech.data.repository
+
+import pe.edu.upc.tukuntech.data.models.LoginRequest
+import pe.edu.upc.tukuntech.data.models.LoginResponse
+import pe.edu.upc.tukuntech.data.remote.AuthService
+import retrofit2.Response
+
+// AuthRepository.kt
+class AuthRepository(private val authService: AuthService) {
+    suspend fun login(username: String, password: String): Result<String> {
+        return try {
+            val response = authService.login(LoginRequest(username, password))
+            if (response.isSuccessful && response.body() != null) {
+                Result.success(response.body()!!.token)
+            } else {
+                Result.failure(Exception("Login fallido: ${response.code()}"))
+            }
+        } catch (e: Exception) {
+            Result.failure(e)
+        }
+    }
+}
