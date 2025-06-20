814e44bc33cfbed323896e8f7eb9ec2cc2b98d73
Oscar <oscaredu01@hotmail.com>
2025-06-19 21:38:43 -0500

    fix: correct errors

--- app/src/main/java/pe/edu/upc/tukuntech/presentation/di/PresentationModule.kt
+++ app/src/main/java/pe/edu/upc/tukuntech/presentation/di/PresentationModule.kt
@@ -5,27 +5,30 @@ import pe.edu.upc.tukuntech.data.repository.AuthRepository
 import pe.edu.upc.tukuntech.presentation.viewmodels.AuthViewModel
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
-
-object PresentationModule {
-    fun getAuthService(): AuthService {
-    return Retrofit.Builder()
-        .baseUrl("https://tukun-tech-platform.onrender.com")
-        .addConverterFactory(GsonConverterFactory.create())
-        .build()
-        .create(AuthService::class.java)
-}
-
-    fun getAuthViewModel(): AuthViewModel {
-        return AuthViewModel(AuthRepository(getAuthService()))
 import pe.edu.upc.tukuntech.data.di.DataModule
 import pe.edu.upc.tukuntech.presentation.viewmodels.CriticalPatientsListViewModel
 import pe.edu.upc.tukuntech.presentation.viewmodels.GetBedsViewModel
 
 object PresentationModule {
-    fun getBedsViewModel(): GetBedsViewModel{
+    fun getAuthService(): AuthService {
+        return Retrofit.Builder()
+            .baseUrl("https://tukun-tech-platform.onrender.com")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(AuthService::class.java)
+    }
+
+    fun getAuthViewModel(): AuthViewModel {
+        return AuthViewModel(AuthRepository(getAuthService()))
+
+    }
+    fun getBedsViewModel(): GetBedsViewModel {
         return GetBedsViewModel(DataModule.getBedsRepository())
     }
-    fun getCriticalPatientsViewModel(): CriticalPatientsListViewModel{
+
+    fun getCriticalPatientsViewModel(): CriticalPatientsListViewModel {
         return CriticalPatientsListViewModel(DataModule.getCriticalPatientsRepository())
     }
 }
+
+
