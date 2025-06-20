fbf5f5925d0ff206e17cfd7769dc68e24203ed89
Erick <erickmaraza20@gmail.com>
2025-06-20 00:28:56 -0500

    feat(auth): handle registration state in AuthViewModel

--- app/src/main/java/pe/edu/upc/tukuntech/presentation/viewmodels/AuthViewModel.kt
+++ app/src/main/java/pe/edu/upc/tukuntech/presentation/viewmodels/AuthViewModel.kt
@@ -5,17 +5,31 @@ import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
-import pe.edu.upc.tukuntech.data.models.LoginResponse
 import pe.edu.upc.tukuntech.data.repository.AuthRepository
-import retrofit2.Response
 
-class AuthViewModel(private val repository: AuthRepository): ViewModel() {
+class AuthViewModel(private val repository: AuthRepository) : ViewModel() {
+
     private val _loginResult = MutableStateFlow<Result<String>?>(null)
     val loginResult: StateFlow<Result<String>?> = _loginResult
+
+    private val _registerResult = MutableStateFlow<Result<Unit>?>(null)
+    val registerResult: StateFlow<Result<Unit>?> = _registerResult
 
     fun login(username: String, password: String) {
         viewModelScope.launch {
             _loginResult.value = repository.login(username, password)
+        }
+    }
+
+    fun register(
+        username: String,
+        password: String,
+        email: String,
+        firstName: String,
+        lastName: String
+    ) {
+        viewModelScope.launch {
+            _registerResult.value = repository.register(username, password, email, firstName, lastName)
         }
     }
 }
