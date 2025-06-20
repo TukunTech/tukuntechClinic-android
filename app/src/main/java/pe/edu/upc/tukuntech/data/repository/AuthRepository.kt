ddbc711276f93f518da2f92270c246e1090c78b2
Erick <erickmaraza20@gmail.com>
2025-06-20 00:28:43 -0500

    feat(auth): add user registration logic to AuthRepository

--- app/src/main/java/pe/edu/upc/tukuntech/data/repository/AuthRepository.kt
+++ app/src/main/java/pe/edu/upc/tukuntech/data/repository/AuthRepository.kt
@@ -2,11 +2,12 @@ package pe.edu.upc.tukuntech.data.repository
 
 import pe.edu.upc.tukuntech.data.models.LoginRequest
 import pe.edu.upc.tukuntech.data.models.LoginResponse
+import pe.edu.upc.tukuntech.data.models.RegisterRequest
 import pe.edu.upc.tukuntech.data.remote.AuthService
-import retrofit2.Response
 
 // AuthRepository.kt
 class AuthRepository(private val authService: AuthService) {
+
     suspend fun login(username: String, password: String): Result<String> {
         return try {
             val response = authService.login(LoginRequest(username, password))
@@ -14,6 +15,26 @@ class AuthRepository(private val authService: AuthService) {
                 Result.success(response.body()!!.token)
             } else {
                 Result.failure(Exception("Login fallido: ${response.code()}"))
+            }
+        } catch (e: Exception) {
+            Result.failure(e)
+        }
+    }
+
+    suspend fun register(
+        username: String,
+        password: String,
+        email: String,
+        firstName: String,
+        lastName: String
+    ): Result<Unit> {
+        return try {
+            val request = RegisterRequest(username, password, email, firstName, lastName)
+            val response = authService.register(request)
+            if (response.isSuccessful) {
+                Result.success(Unit)
+            } else {
+                Result.failure(Exception("Registro fallido: ${response.code()}"))
             }
         } catch (e: Exception) {
             Result.failure(e)
