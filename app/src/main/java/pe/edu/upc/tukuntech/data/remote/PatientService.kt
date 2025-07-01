1d2171a5cb8c516cafcebe16971fd76d3be01421
Nicolini03 <U202218791@upc.edu.pe>
2025-07-01 18:35:57 -0500

    feat(PatientService): define endpoint for patient registration

--- app/src/main/java/pe/edu/upc/tukuntech/data/remote/PatientService.kt
+++ app/src/main/java/pe/edu/upc/tukuntech/data/remote/PatientService.kt
@@ -1,9 +1,15 @@
 package pe.edu.upc.tukuntech.data.remote
 
 import pe.edu.upc.tukuntech.data.models.PatientEntity
+import retrofit2.Response
+import retrofit2.http.Body
 import retrofit2.http.GET
+import retrofit2.http.POST
 
 interface PatientService {
     @GET("patients/patients")
     suspend fun getAllPatients(): List<PatientEntity>
+
+    @POST("/api/v1/patients")
+    suspend fun createPatient(@Body patient: PatientEntity): Response<Unit>
 }
