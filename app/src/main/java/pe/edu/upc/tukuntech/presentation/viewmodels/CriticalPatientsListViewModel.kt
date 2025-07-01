5b571d078634a29eb480409f8e573dfbcd87019c
oscaredu1226 <oscaredu01@hotmail.com>
2025-07-01 11:41:34 -0500

    feat(viewmodel): add loading state handling to CriticalPatientsListViewModel

--- app/src/main/java/pe/edu/upc/tukuntech/presentation/viewmodels/CriticalPatientsListViewModel.kt
+++ app/src/main/java/pe/edu/upc/tukuntech/presentation/viewmodels/CriticalPatientsListViewModel.kt
@@ -8,20 +8,28 @@ import kotlinx.coroutines.launch
 import pe.edu.upc.tukuntech.data.repository.CriticalPatientsRepository
 import pe.edu.upc.tukuntech.domain.models.CriticalPatient
 
-class CriticalPatientsListViewModel(private val patientsCriticalRepository: CriticalPatientsRepository): ViewModel() {
+class CriticalPatientsListViewModel(
+    private val patientsCriticalRepository: CriticalPatientsRepository
+) : ViewModel() {
+
     private val _criticalPatients = MutableStateFlow<List<CriticalPatient>>(emptyList())
-    val criticalPatients : StateFlow<List<CriticalPatient>> = _criticalPatients
+    val criticalPatients: StateFlow<List<CriticalPatient>> = _criticalPatients
+
+    private val _isLoading = MutableStateFlow(true)
+    val isLoading: StateFlow<Boolean> = _isLoading
 
     fun fetchCriticalPatients() {
         viewModelScope.launch {
+            _isLoading.value = true
             _criticalPatients.value = patientsCriticalRepository.fetchCriticalPatients()
+            _isLoading.value = false
         }
     }
+
     fun deleteCriticalPatients(criticalPatient: CriticalPatient) {
         viewModelScope.launch {
             patientsCriticalRepository.deleteCriticalPatient(criticalPatient)
             fetchCriticalPatients()
         }
     }
-
 }
