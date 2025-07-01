9c8b31892cc0889ae9d5ed199d679f6db6e26378
oscaredu1226 <oscaredu01@hotmail.com>
2025-07-01 12:44:51 -0500

    style(patient-list): enhance dropdown styling with consistent spacing and bold labels

--- app/src/main/java/pe/edu/upc/tukuntech/domain/models/PatientWithBed.kt
+++ app/src/main/java/pe/edu/upc/tukuntech/domain/models/PatientWithBed.kt
@@ -3,5 +3,10 @@ package pe.edu.upc.tukuntech.domain.models
 data class PatientWithBed(
     val id: Int,
     val fullName: String,
-    val bedName: String?
+    val bedName: String? = null,
+    val dni: String,
+    val age: Int,
+    val gender: String,
+    val bloodType: String,
+    val nationality: String
 )
